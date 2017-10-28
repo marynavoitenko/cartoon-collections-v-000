2cbec31009289dd44d9ec3d42a10a180fe6618e1
Maryna Voitenko <maryna.voitenko@gmail.com>
2017-10-28 04:01:47 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarfs)
+  dwarfs.each_with_index {|dwarf, i| puts dwarfs[i]}
 end
 
 def summon_captain_planet# code an argument here

9a741c109cdba4c2a35d0021bb9110c207bafd79
Maryna Voitenko <maryna.voitenko@gmail.com>
2017-10-28 04:18:32 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,7 @@ def long_planeteer_calls(calls)
   calls.any? {|call| call.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
+  array.detect {|i| cheese_types.include?(i)}
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
